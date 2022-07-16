d60f0600d563f206bd5a92832f863db114d896cc
IanZGray <ianzacharygray@gmail.com>
2022-07-16 18:32:09 -0400

    Navbar hover

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 // import { LinkContainer } from 'react-router-dom';
+import './navigationbar.css'
 
 function NavigationBar() {
+
+  // useState to adjust this? useEffect?
+  // const currentLocation = window.location.href;
+  // const menuItem = document.querySelectorAll('nav-item');
+  // const menuLength = menuItem.length
+  // for (let i = 0; i<menuLength; i++){
+  //   if(menuItem[i].href === currentLocation){
+  //     menuItem[i].className = "active"
+  //   }
+  // }
     return (
-      <Nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Nav className="navbar navbar-expand-lg navbar-light bg-info">
         <a className="navbar-brand" href="Home">BadBank</a>
         <button 
             className="navbar-toggler" 
@@ -20,25 +31,26 @@ function NavigationBar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
           <li className="nav-item">
-              <a className="nav-link" href="home">Home</a>
+              <a className="nav-link hovertext" data-hover="Home page link" href="home">Home</a>
+              
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="createAccount">Create Account</a>
+              <a className="nav-link hovertext" data-hover="Create your account here" href="createAccount">Create Account</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="login">Login</a>
+              <a className="nav-link hovertext" data-hover="Log in here" href="login">Login</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="deposit">Deposit</a>
+              <a className="nav-link hovertext" data-hover="Make a deposit here" href="deposit">Deposit</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="withdraw">Withdraw</a>
+              <a className="nav-link hovertext" data-hover="Withdraw money Here" href="withdraw">Withdraw</a>
             </li>
             {/* <li className="nav-item">
-              <a className="nav-link" href="balance">Balance</a>
+              <a className="nav-link hovertext" data-hover="View your balance here" href="balance">Balance</a>
             </li> */}
             <li className="nav-item">
-              <a className="nav-link" href="data">AllData</a>
+              <a className="nav-link hovertext" data-hover="All user data is here"  href="data">AllData</a>
             </li>          
           </ul>
         </div>
